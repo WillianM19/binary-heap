e35ce0861534d007476a76c4200b14af148230c9
WillianM19 <willianm1928@gmail.com>
2024-11-10 23:28:56 -0300

    feat: Ajustes na classe binaryheap e finalizando testes

--- BinaryHeap.py
+++ BinaryHeap.py
@@ -1,22 +1,29 @@
+import copy
 import math
 
 class BinaryHeap:
-    def __init__(self, array=[None, 50, 40, 20, 15, 16, 13, None, None]):
-        self.array = array
+    # def __init__(self, array=[None, 50, 40, 20, 15, 16, 13, None, None]):
+    #     self.array = array
 
+    #     #Contador de elementos preechidos
+    #     self.size = len([x for x in array if x is not None])
+
+    def __init__(self, capacity):
+        # Array com Nones de acordo com a capacidade
+        self.array = [None] * (capacity + 1)
         #Contador de elementos preechidos
-        self.size = len([x for x in array if x is not None]) 
+        self.size = 0
 
 
     def insert(self, novo):
         # Verifica se ainda há espaço na heap
         if self.size < len(self.array):
 
-            # Adiciona o novo elemento na próxima posição disponível
+            # Adiciona o novo elemento no final do heap
             self.array[self.size + 1] = novo
             self.size += 1
 
-            # Ajusta a posição do novo elemento para manter a propriedade da heap
+            # Sobe ele para ordenar
             self._subir(self.size)
         else:
             print("Overflow: A heap está cheia.")
@@ -26,17 +33,56 @@ class BinaryHeap:
             # Salva o elemento raiz
             raiz = self.array[1]
 
-            print(f"Movendo ultimo para raiz - {self.array[self.size]} <-> {self.array[1]}")
-
-            # Pega o ultimo elemento e move para a raiz
+            # Move o último elemento para a raiz
             self.array[1] = self.array[self.size]
+            self.array[self.size] = None  # Adicona None para indicar que o elemento foi removido
+            self.size -= 1
 
-
+            # Desce ele para ordenar
             self._descer(1, self.size)
 
             return raiz
         else:
                 print("Underflow: A heap está vazia.")
+
+    def change_priority(self, index, prioridade):
+        # Verifica se o índice está dentro dos limites do heap
+        if 1 <= index <= self.size:
+
+            # Salva prioridade antiga e atualiza para nova
+            prioridade_antes = self.array[index]
+            self.array[index] = prioridade
+
+            # Sobe ou desce dependendo da prioridade nova
+            if prioridade > prioridade_antes:
+                self._subir(index)
+            else:
+                self._descer(index, self.size)
+
+        else:
+            print("Índice fora do limite do heap.")
+
+
+    def get_high_priority(self):
+        return self.array[1] if self.size > 0 else None
+
+    def heap_sort(self):
+        print("Criando cópia...")
+        # Cópia de si mesmo para não alterar a heap original
+        heap_copy = copy.deepcopy(self)
+        print("Heap antes de ordenar:", heap_copy.array)
+
+        # Loop removendoo elementos do heap e adicionando no array
+        sorted_list = []
+        while heap_copy.size > 0:
+            high_priority = heap_copy.remove()
+            sorted_list.append(high_priority)
+            print("Estado após remoção:", heap_copy.array)
+
+        print("Resultado do Heap Sort:", sorted_list)
+
+    def display_heap(self):
+        print(self.array)
 
     def _subir(self, i):
         # Pega o indice do pai
@@ -45,7 +91,6 @@ class BinaryHeap:
         if pai >= 1:
             # Se for maior que o elemento pai
             if self.array[i] > self.array[pai]:
-                print(f"Subindo - {self.array[i]} <-> {self.array[pai]}")
 
                 # Swap entre pai e filho
                 aux = self.array[pai]
@@ -64,7 +109,7 @@ class BinaryHeap:
                     filho += 1
 
             if self.array[i] < self.array[filho]:
-                print(f"Descendo - {self.array[i]} <-> {self.array[filho]}")
+
                 # Swap entre pai e filho
                 aux = self.array[i]
                 self.array[i] = self.array[filho]
@@ -72,6 +117,3 @@ class BinaryHeap:
 
                 # Recursão com o filho que mudou para elemento pai.
                 self._descer(filho, n)
-
-    def __str__(self):
-        return str(self.array)
--- main.py
+++ main.py
@@ -1,19 +1,177 @@
 from BinaryHeap import BinaryHeap
 
-arvore = BinaryHeap()
+print("\n========== Conjunto 1: Dados Aleatórios Pequenos ==========\n")
+data1 = [10, 5, 20, 1, 15, 30, 25]
+conjunto1 = BinaryHeap(len(data1))
+#                      ^Passo a capacidade com base no tamanho dos dados
 
-print("\nArvore inicial:\n")
-print(arvore, "\n")
+# Inserções sequenciais
+for value in data1:
+    print(f"Inserindo o valor {value}")
+    conjunto1.insert(value)
+    conjunto1.display_heap()
+    print("\n")
 
-print("🆕 Adicionando número 1000:\n")
-arvore.adicionar(1000)
-print("Resultado:", arvore, "\n")
+# Alterações de prioridade
+print("Alterando o índice 3 para prioridade 50")
+print(f"Antes:")
+conjunto1.display_heap()
+conjunto1.change_priority(3, 50)
+print(f"Depois:")
+conjunto1.display_heap()
 
-print("🆕 Adicionando número 1999:\n")
-arvore.adicionar(1999)
-print("Resultado:", arvore, "\n")
+# Alterações de prioridade
+print("\nAlterando o índice 1 para prioridade 8")
+print(f"Antes:")
+conjunto1.display_heap()
+conjunto1.change_priority(1, 8)
+print(f"Depois:")
+conjunto1.display_heap()
 
-print("⏪ Removendo elemento raiz:\n")
-elemento_removido = arvore.remover()
-print("Resultado:", arvore, "\n")
-print(f"🆗 Raiz removida: {elemento_removido}")
+# Remoções
+for i in range(3):
+    print("\nRemovendo elemento de alta prioridade...")
+    conjunto1.remove()
+    conjunto1.display_heap()
+
+# HeapSort
+print("\nHeapSort (removendo todos os elementos e retornar eles ordenados):")
+conjunto1.heap_sort()
+
+# Selecionar alta prioridade
+print("\nElemento de alta prioridade:", conjunto1.get_high_priority())
+
+input("\nENTER - Próximo conjunto(2/4)\n")
+
+#----------------------------------------------
+
+print("\n========== Conjunto 2: Sequência Crescente ==========\n")
+data2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+conjunto2 = BinaryHeap(len(data2))
+
+# Inserções sequenciais
+for value in data2:
+    print(f"Inserindo o valor {value}")
+    conjunto2.insert(value)
+    conjunto2.display_heap()
+    print("\n")
+
+# Alterações de prioridade
+print("Alterando o índice 4 para prioridade 15")
+print(f"Antes:")
+conjunto2.display_heap()
+conjunto2.change_priority(4, 15)
+print(f"Depois:")
+conjunto2.display_heap()
+
+# Alterações de prioridade
+print("\nAlterando o índice 8 para prioridade 3")
+print(f"Antes:")
+conjunto2.display_heap()
+conjunto2.change_priority(8, 3)
+print(f"Depois:")
+conjunto2.display_heap()
+
+# Remoções
+for i in range(5):
+    print("\nRemovendo elemento de alta prioridade...")
+    conjunto2.remove()
+    conjunto2.display_heap()
+
+# HeapSort
+print("\nHeapSort (removendo todos os elementos e retornar eles ordenados):")
+conjunto2.heap_sort()
+
+# Selecionar alta prioridade
+print("\nElemento de alta prioridade:", conjunto2.get_high_priority())
+
+input("\nENTER - Próximo conjunto(3/4)\n")
+
+#----------------------------------------------
+
+print("\n========== Conjunto 3: Sequência Decrescente ==========\n")
+data3 = [50, 40, 30, 20, 10, 5, 3]
+conjunto3 = BinaryHeap(len(data3))
+
+# Inserções sequenciais
+for value in data3:
+    print(f"Inserindo o valor {value}")
+    conjunto3.insert(value)
+    conjunto3.display_heap()
+    print("\n")
+
+# Alterações de prioridade
+print("Alterando o índice 2 para prioridade 60")
+print(f"Antes:")
+conjunto3.display_heap()
+conjunto3.change_priority(2, 60)
+print(f"Depois:")
+conjunto3.display_heap()
+
+# Alterações de prioridade
+print("\nAlterando o índice 5 para prioridade 1")
+print(f"Antes:")
+conjunto3.display_heap()
+conjunto3.change_priority(5, 1)
+print(f"Depois:")
+conjunto3.display_heap()
+
+# Remoções
+for i in range(3):
+    print("\nRemovendo elemento de alta prioridade...")
+    conjunto3.remove()
+    conjunto3.display_heap()
+
+# HeapSort
+print("\nHeapSort (removendo todos os elementos e retornar eles ordenados):")
+conjunto3.heap_sort()
+
+# Selecionar alta prioridade
+print("\nElemento de alta prioridade:", conjunto3.get_high_priority())
+
+input("\nENTER - Próximo conjunto(4/4)\n")
+
+#----------------------------------------------
+
+print("\n========== Conjunto 4: Dados Aleatórios Maiores ==========\n")
+data4 = [13, 26, 19, 17, 24, 31, 22, 11, 8, 20, 5, 27, 18]
+conjunto4 = BinaryHeap(len(data4))
+
+# Inserções sequenciais
+for value in data4:
+    print(f"Inserindo o valor {value}")
+    conjunto4.insert(value)
+    conjunto4.display_heap()
+    print("\n")
+
+# Alterações de prioridade
+print("Alterando o índice 7 para prioridade 35")
+print(f"Antes:")
+conjunto4.display_heap()
+conjunto4.change_priority(7, 35)
+print(f"Depois:")
+conjunto4.display_heap()
+
+# Alterações de prioridade
+print("\nAlterando o índice 10 para prioridade 12")
+print(f"Antes:")
+conjunto4.display_heap()
+conjunto4.change_priority(10, 12)
+print(f"Depois:")
+conjunto4.display_heap()
+
+# Remoções
+for i in range(4):
+    print("\nRemovendo elemento de alta prioridade...")
+    conjunto4.remove()
+    conjunto4.display_heap()
+
+# HeapSort
+print("\nHeapSort (removendo todos os elementos e retornar eles ordenados):")
+conjunto4.heap_sort()
+
+# Selecionar alta prioridade
+print("\nElemento de alta prioridade:", conjunto4.get_high_priority())
+
+input("\nENTER - Finalizar\n")
+
